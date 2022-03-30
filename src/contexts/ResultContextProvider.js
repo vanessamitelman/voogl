db9fb78826d06b422da36d6446bb1edb309434d8
Vanessa Mitelman <vanessamitelman@gmail.com>
2022-03-30 15:14:35 +0300

    updated

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -10,29 +10,29 @@ export const ResultContextProvider = ({ children }) => {
   // '/image' '/videos' etc...
   const getResults = async (type) => {
     setIsLoading(true);
-try{
-const response = await fetch(`${baseUrl}${type}`, {
-      method: 'GET',
-      headers: {
-        'X-User-Agent': 'desktop',
-        'X-Proxy-Location': 'EU',
-        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
+    try {
+      const response = await fetch(`${baseUrl}${type}`, {
+        method: 'GET',
+        headers: {
+          'X-User-Agent': 'desktop',
+          'X-Proxy-Location': 'EU',
+          'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
+        }
+      });
+      const data = await response.json();
+      if (type.includes('/news')) {
+        setResults(data.entries);
+      } else if (type.includes('/image')) {
+        setResults(data.image_results);
+      } else {
+        setResults(data.results);
       }
-    });
-    const data = await response.json();
-    if (type.includes('/news')) {
-      setResults(data.entries);
-    } else if (type.includes('/image')) {
-      setResults(data.image_results);
-    } else {
-      setResults(data.results);
+
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
     }
-
-    setIsLoading(false);
-
-}catch(e){    console.log(e)}
-
   };
   return (
     <ResultContext.Provider
